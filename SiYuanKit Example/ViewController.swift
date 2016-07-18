72cf358bcbab0762fe692e2e40ef3f400d9a4b2d
yansong li <857367901@qq.com>
2016-07-17 23:49:42 -0400

    First Init

---
+++ SiYuanKit Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SiYuanKit Example
+//
+//  Created by yansong li on 2016-07-17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
