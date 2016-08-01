805cf04722a43687a1775352f2af0079ceaef641
yansong li <857367901@qq.com>
2016-08-01 11:31:04 -0400

    Add Section data to TableManager

--- SiYuanKit Example/ViewController.swift
+++ SiYuanKit Example/ViewController.swift
@@ -29,10 +29,16 @@ class ViewController: UIViewController {
     let row2 = Row(title: "Row2", description: "This is a quite long sentence and i hope that this sentence could be truncated if needed, this means that I could happily go to sleep without any worries", cellType: ItemCell.self, cellIdentifier: "item")
     let testRows = [row1, row2]
     
-    let testRow2 = [row2]
-    tableManager.rows = testRows
+    let row3 = row2
+    let row4 = row2
+    
+    let sections = Section(title: "Section1", rows: testRows)
+    
+    let sec2 = Section(title: "Section2", rows: [row3, row4])
+    
+    tableManager.data = .SingleSection(testRows)
     GCDQueue.main.after(when: 3.0) { 
-      self.tableManager.rows = testRow2
+      self.tableManager.data = .MultiSection([sections, sec2])
     }
   }
 
--- Sources/UI/StaticTableView/TableViewManager.swift
+++ Sources/UI/StaticTableView/TableViewManager.swift
@@ -11,6 +11,12 @@ import Foundation
 /// Manager to handle TableView related events.
 public class TableViewManager: NSObject {
   
+  /// Representation of the style of tableViewData.
+  public enum TableViewStyle {
+    case SingleSection([Row])
+    case MultiSection([Section])
+  }
+  
   /// The Identifiers registered to the `tableView`.
   public var registeredCellIdentifiers: [String] = []
   
@@ -29,18 +35,28 @@ public class TableViewManager: NSObject {
     }
   }
   
-  /// An array of Row data.
-  public var rows: [Row] = [] {
+  /// The data for this tableViewManager.
+  public var data: TableViewStyle = .SingleSection([]) {
     didSet {
       refreshTableView()
     }
   }
-  
+}
+
+// MARK: Helpers
+extension TableViewManager {
   /// Reload tableView and register cell.
   private func refreshTableView() {
-    // Execute on main thread.
-    let rows =
-      self.rows.filter { !self.registeredCellIdentifiers.contains($0.cellIdentifier) }
+    var rows: [Row] = []
+    
+    switch data {
+    case .SingleSection(let r):
+      rows = r.filter { !self.registeredCellIdentifiers.contains($0.cellIdentifier) }
+    case .MultiSection(let s):
+      rows = s.flatMap { $0.rows }.filter {
+        !self.registeredCellIdentifiers.contains($0.cellIdentifier)
+      }
+    }
     rows.forEach { (row) in
       if self.registeredCellIdentifiers.contains(row.cellIdentifier) {
         return
@@ -50,38 +66,92 @@ public class TableViewManager: NSObject {
     }
     self.tableView?.reloadData()
   }
+  
+  /// Return row for specified index.
+  private func rowForIndexPath(_ index: IndexPath) -> Row? {
+    switch data {
+    case .SingleSection(let r):
+      if r.count > index.row {
+        return r[index.row]
+      }
+    case .MultiSection(let s):
+      if s.count > index.section {
+        let selectSection = s[index.section]
+        if selectSection.rows.count > index.row {
+          return selectSection.rows[index.row]
+        }
+      }
+    }
+    return nil
+  }
 }
 
 extension TableViewManager: UITableViewDataSource {
   
   public func tableView(_ tableView: UITableView,
       numberOfRowsInSection section: Int) -> Int {
-    return rows.count
+    switch data {
+    case .SingleSection(let r):
+      return r.count
+    case .MultiSection(let s):
+      return s[section].rows.count
+    }
   }
   
   public func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
+    switch data {
+    case .SingleSection(_):
+      return 1
+    case .MultiSection(let s):
+      return s.count
+    }
   }
   
   public func tableView(_ tableView: UITableView,
              cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let row = rows[indexPath.row]
-    let cell = tableView.dequeueReusableCell(withIdentifier: row.cellIdentifier,
-                                             for: indexPath)
-    if let staticCell = cell as? StaticCellType {
-      staticCell.configure(row: row)
+    if let row = rowForIndexPath(indexPath) {
+      let cell = tableView.dequeueReusableCell(withIdentifier: row.cellIdentifier,
+                                                          for: indexPath)
+      if let staticCell = cell as? StaticCellType {
+        staticCell.configure(row: row)
+      }
       return cell
-    } else {
-      return UITableViewCell()
     }
+    return UITableViewCell()
+  }
+  
+  public func tableView(_ tableView: UITableView,
+    titleForHeaderInSection section: Int) -> String? {
+    switch data {
+    case .SingleSection(_):
+      return nil
+    case .MultiSection(let s):
+      return s[section].title
+    }
+  }
+  
+  public func tableView(_ tableView: UITableView,
+   heightForHeaderInSection section: Int) -> CGFloat {
+    return UITableViewAutomaticDimension
+  }
+  
+  public func tableView(_ tableView: UITableView,
+   heightForFooterInSection section: Int) -> CGFloat {
+    return UITableViewAutomaticDimension
+  }
+  
+  public func tableView(_ tableView: UITableView,
+    titleForFooterInSection section: Int) -> String? {
+    return nil
   }
 }
 
 extension TableViewManager: UITableViewDelegate {
   public func tableView(_ tableView: UITableView,
            didSelectRowAt indexPath: IndexPath) {
-    let row = rows[indexPath.row]
-    row.action?()
+    if let row = self.rowForIndexPath(indexPath) {
+      row.action?()
+    }
     tableView.deselectRow(at: indexPath, animated: true)
   }
 }
