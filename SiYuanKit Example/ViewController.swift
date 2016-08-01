a528c02dac52ff84cfd8ad0313fc0344d2127d1a
yansong li <857367901@qq.com>
2016-07-31 21:40:13 -0400

    Add TableViewManager to simplify static tableView operation.

--- SiYuanKit Example/ViewController.swift
+++ SiYuanKit Example/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 import SiYuan
 
 class ViewController: UIViewController {
+  
+  let tableView: UITableView = UITableView()
+  let tableManager: TableViewManager = TableViewManager()
+  
+  override func loadView() {
+    self.view = tableView
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableManager.tableView = tableView
+    let row1:Row = Row(title: "Row1", description: "Lala", image: nil, action: nil, cellIdentifier: "cell1")
+    let row2 = Row(title: "Row22", description: "dklskjdlajfisdjflkasdjfias")
+    let testRows = [row1, row2]
+    tableManager.rows = testRows
+    
+    
   }
 
   override func didReceiveMemoryWarning() {
---
+++ Sources/UI/StaticTableView/TableViewManager.swift
@@ -0,0 +1,87 @@
+//
+//  TableViewManager.swift
+//  SiYuanKit
+//
+//  Created by yansong li on 2016-07-31.
+//
+//
+
+import Foundation
+
+/// Manager to handle TableView related events.
+public class TableViewManager: NSObject {
+  /**
+    tableView object managed by this Manager.
+   */
+  public var tableView: UITableView? {
+    willSet {
+      registeredCellIdentifiers = []
+    }
+    
+    didSet {
+      tableView?.delegate = self
+      tableView?.dataSource = self
+      refreshTableView()
+    }
+  }
+  
+  /// The Identifiers registered to the `tableView`.
+  public var registeredCellIdentifiers: [String] = []
+  
+  /// An array of Row data.
+  public var rows: [Row] = [] {
+    didSet {
+      refreshTableView()
+    }
+  }
+  
+  /// Reload tableView and register cell.
+  private func refreshTableView() {
+    // Execute on main thread.
+    let rows =
+      self.rows.filter { !self.registeredCellIdentifiers.contains($0.cellIdentifier) }
+    rows.forEach { (row) in
+      if self.registeredCellIdentifiers.contains(row.cellIdentifier) {
+        return
+      }
+      self.registeredCellIdentifiers.append(row.cellIdentifier)
+      self.tableView?.register(row.cellType, forCellReuseIdentifier: row.cellIdentifier)
+    }
+    self.tableView?.reloadData()
+  }
+}
+
+extension TableViewManager: UITableViewDataSource {
+  
+  public func tableView(_ tableView: UITableView,
+      numberOfRowsInSection section: Int) -> Int {
+    return rows.count
+  }
+  
+  public func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  public func tableView(_ tableView: UITableView,
+             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let row = rows[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: row.cellIdentifier,
+                                             for: indexPath)
+    if let staticCell = cell as? StaticCellType {
+      staticCell.configure(row: row)
+      return cell
+    } else {
+      return UITableViewCell()
+    }
+  }
+}
+
+extension TableViewManager: UITableViewDelegate {
+  public func tableView(_ tableView: UITableView,
+           didSelectRowAt indexPath: IndexPath) {
+    let row = rows[indexPath.row]
+    if let action = row.action {
+      action()
+    }
+  }
+}
