38e3db88832378a4a81ddc2f19d7fd7b260d66e4
yansong li <857367901@qq.com>
2016-08-14 19:57:44 -0400

    Make addAutolayoutSubView public

--- Sources/UI/Extension/UIViewExtensions.swift
+++ Sources/UI/Extension/UIViewExtensions.swift
@@ -11,7 +11,7 @@ import Foundation
 // MARK: Auto layout.
 extension UIView {
   /// Add a view as subview, turn off autoresizingmask.
-  func addAutoLayoutSubView(_ view: UIView) {
+  public func addAutoLayoutSubView(_ view: UIView) {
     view.translatesAutoresizingMaskIntoConstraints = false
     self.addSubview(view)
   }
